38d48c900b0650d007575db78e039fb5f87b0d84
hoanghaiphongg <hoanghaiphongngu2@gmail.com>
2024-11-29 11:32:47 +0700

    update province into coffeeshop

--- src/migrations/migration-create-coffeeshop.js
+++ src/migrations/migration-create-coffeeshop.js
@@ -18,6 +18,36 @@ module.exports = {
                 type: Sequelize.STRING(100),
                 allowNull: false,
             },
+            province_vie: {
+                type: Sequelize.ENUM(
+                    'An Giang', 'Bà Rịa - Vũng Tàu', 'Bắc Giang', 'Bắc Kạn', 'Bạc Liêu', 'Bắc Ninh',
+                    'Bến Tre', 'Bình Định', 'Bình Dương', 'Bình Phước', 'Bình Thuận', 'Cà Mau',
+                    'Cần Thơ', 'Cao Bằng', 'Đà Nẵng', 'Đắk Lắk', 'Đắk Nông', 'Điện Biên', 'Đồng Nai',
+                    'Đồng Tháp', 'Gia Lai', 'Hà Giang', 'Hà Nam', 'Hà Nội', 'Hà Tĩnh', 'Hải Dương',
+                    'Hải Phòng', 'Hậu Giang', 'Hòa Bình', 'Hưng Yên', 'Khánh Hòa', 'Kiên Giang',
+                    'Kon Tum', 'Lai Châu', 'Lâm Đồng', 'Lạng Sơn', 'Lào Cai', 'Long An', 'Nam Định',
+                    'Nghệ An', 'Ninh Bình', 'Ninh Thuận', 'Phú Thọ', 'Phú Yên', 'Quảng Bình',
+                    'Quảng Nam', 'Quảng Ngãi', 'Quảng Ninh', 'Quảng Trị', 'Sóc Trăng', 'Sơn La',
+                    'Tây Ninh', 'Thái Bình', 'Thái Nguyên', 'Thanh Hóa', 'Thừa Thiên Huế', 'Tiền Giang',
+                    'TP Hồ Chí Minh', 'Trà Vinh', 'Tuyên Quang', 'Vĩnh Long', 'Vĩnh Phúc', 'Yên Bái'
+                ),
+                allowNull: false,
+            },
+            province_jap: {
+                type: Sequelize.ENUM(
+                    'アンザン', 'バリア=ブンタウ', 'バックザン', 'バックカン', 'バックリエウ', 'バクニン',
+                    'ベンチェ', 'ビンディン', 'ビンズオン', 'ビンフオック', 'ビントゥアン', 'カマウ',
+                    'カントー', 'カオバン', 'ダナン', 'ダクラク', 'ダクノン', 'ディエンビエン', 'ドンナイ',
+                    'ドンタップ', 'ザライ', 'ハザン', 'ハナム', 'ハノイ', 'ハティン', 'ハイズオン',
+                    'ハイフォン', 'ハウザン', 'ホアビン', 'フンイエン', 'カンホア', 'キエンザン',
+                    'コンツム', 'ライチャウ', 'ラムドン', 'ランソン', 'ラオカイ', 'ロンアン', 'ナムディン',
+                    'ゲアン', 'ニンビン', 'ニントゥアン', 'フート', 'フーイエン', 'クアンビン',
+                    'クアンナム', 'クアンガイ', 'クアンニン', 'クアンチ', 'ソクチャン', 'ソンラ',
+                    'タイニン', 'タイビン', 'タイグエン', 'タインホア', 'トゥアティエンフエ', 'ティエンザン',
+                    'ホーチミン市', 'チャビン', 'トゥエンクアン', 'ビンロン', 'ビンフック', 'イエンバイ'
+                ),
+                allowNull: false,
+            },
             address: {
                 type: Sequelize.STRING(255),
                 allowNull: false,
